9ff21bf11d4b685f2024f21fc4a31ce51a108bd9
Tony Butler <tony@Tonys-MacBook-Pro.local>
2016-01-30 14:39:12 +0000

    Remove hello template

--- client/js/main.js
+++ client/js/main.js
@@ -1,33 +1,3 @@
-
-
-// individual document page
-/*
-Router.route('/documents/:_id', function () {
-    console.log("you hit /documents  "+this.params._id);
-    Session.set("docid", this.params._id);
-    this.render("navbar", {to:"header"});
-    this.render("docItem", {to:"main"});
-});
-*/
-
-
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-    counter: function () {
-        return Session.get('counter');
-    }
-});
-
-
-Template.hello.events({
-    'click button': function () {
-        // increment the counter when button is clicked
-        Session.set('counter', Session.get('counter') + 1);
-    }
-});
-
 Template.registerHelper('Schemas', function() {
     return Schemas;
 });
